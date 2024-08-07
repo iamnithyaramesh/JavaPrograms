8377db1e412556bd8185155c384e07c5bbd46c11
iamnithyaramesh <nithyasri.ramesh2912@gmail.com>
2024-08-07 13:51:00 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,47 @@
+import java.util.*;
+class Test{
+public static void Main(String []args)
+{
+    System.out.println("Hello world");
+}
+}
+
+class HelloWorld{
+    public static void main(String []args)
+    {
+        System.out.println("This is the Hello World Program");
+        int i=7;
+        float f=7;
+        double d=7;
+        byte b=2;
+        char a='H';
+        short s=57;
+        long l=128;
+        String string="Nithu";
+        String str=new String("Nithya");
+
+        int[] c={10,20,30,40};
+        System.out.println(Arrays.toString(c));
+        c[2]=50;
+        System.out.println(Arrays.toString(c));
+
+
+
+        System.out.println(i);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(b);
+        System.out.println(a);
+        System.out.println(s);
+        System.out.println(l);
+        System.out.println(string);
+        System.out.println(str);
+    
+ 
+
+    }
+
+}
+
+
+
